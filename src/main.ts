4e38ba04857c8977210734860aaf84a2c5ed7259
yangc <395709505@qq.com>
2020-12-31 09:50:33 +0800

    测试增加注释

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { setupAntd } from '/@/plugins/'
 import router from './router'
 import store from './store'
 
-import './index.scss'
+import './index.scss' //引入通用样式
 const app = createApp(App)
 setupAntd(app)
 
