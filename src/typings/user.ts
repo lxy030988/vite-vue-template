9b0c5d7ec12de5cc02d954eacdd2f335ccf27408
lixiangyu <654762937@qq.com>
2021-04-16 16:18:11 +0800

    tsconfig 修改

--- src/typings/user.ts
+++ src/typings/user.ts
@@ -30,3 +30,12 @@ export interface HomeState {
   sliders: Slider[]
   lessons: Lessons
 }
+
+interface Person {
+  name: string
+  age: number
+}
+
+type K1 = keyof Person // "name" | "age"
+type K2 = keyof Person[] // "length" | "toString" | "pop" | "push" | "concat" | "join"
+type K3 = keyof { [x: string]: Person } // string | number
