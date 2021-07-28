40bd562d4fd47ad4590238ceb56e898e84cc4b7e
UILcoder <836219401@qq.com>
2021-07-28 15:52:39 +0800

    授权管理，授权状态和授权信息输入框无提示文字

--- src/views/AuthorizationManagement/modules/Filter/index.vue
+++ src/views/AuthorizationManagement/modules/Filter/index.vue
@@ -1,25 +1,60 @@
 <template>
-  <a-card :bordered="false" class="jc-tabfilter">
-    <a-form layout="inline" :model="formState">
+  <a-card
+    :bordered="false"
+    class="jc-tabfilter"
+  >
+    <a-form
+      layout="inline"
+      :model="formState"
+      :label-col="labelCol"
+      :wrapper-col="wrapperCol"
+    >
       <a-form-item label="批次日期">
-        <a-date-picker v-model:value="formState.batchTime" format="YYYY-MM-DD" value-format="YYYY-MM-DD" :show-time="false" />
+        <a-date-picker
+          v-model:value="formState.batchTime"
+          format="YYYY-MM-DD"
+          value-format="YYYY-MM-DD"
+          :show-time="false"
+        />
       </a-form-item>
       <a-form-item label="授权状态">
-        <a-select v-model:value="formState.licenseStatus">
-          <a-select-option v-for="item in LICENSE_STATUSES.VALUES" :key="item.value" :value="item.value">{{item.label}}</a-select-option>
+        <a-select
+          v-model:value="formState.licenseStatus"
+          placeholder="全部状态"
+        >
+          <a-select-option
+            v-for="item in LICENSE_STATUSES.VALUES"
+            :key="item.value"
+            :value="item.value"
+          >{{item.label}}</a-select-option>
         </a-select>
       </a-form-item>
       <a-form-item label="授权信息">
-        <a-input v-model:value="formState.licenseInfo"></a-input>
+        <a-input
+          v-model:value="formState.licenseInfo"
+          placeholder="请输入批次号/合同号/购买公司"
+        ></a-input>
       </a-form-item>
       <a-form-item label="授权日期">
-        <a-range-picker v-model:value="formState.date" value-format="YYYY-MM-DD HH:mm:ss" @change="changeDate" />
-      </a-form-item>
-      <a-form-item class="jc-tabfilter-btns">
-        <a-button type="primary" html-type="submit" @click="onSubmit">查询</a-button>
-        <a-button class="jc-ml" @click="resetFields">重置</a-button>
+        <a-range-picker
+          v-model:value="formState.date"
+          value-format="YYYY-MM-DD HH:mm:ss"
+          @change="changeDate"
+        />
       </a-form-item>
     </a-form>
+    <div class="jc-tabfilter-btns">
+      <a-button
+        type="primary"
+        html-type="submit"
+        @click="onSubmit"
+      >查询</a-button>
+      <a-button
+        class="jc-ml"
+        @click="resetFields"
+      >重置</a-button>
+    </div>
+
   </a-card>
 </template>
 
@@ -34,7 +69,7 @@ export default defineComponent({
   setup(props, { emit }) {
     let formState = reactive({
       licenseInfo: '',
-      licenseStatus: '',
+      licenseStatus: undefined,
       batchTime: '',
       date: [],
       startTime: '',
@@ -58,10 +93,21 @@ export default defineComponent({
       // console.log('changeDate', v)
     }
 
-    return { formState, resetFields, onSubmit, changeDate, LICENSE_STATUSES }
+    return {
+      formState,
+      resetFields,
+      onSubmit,
+      changeDate,
+      LICENSE_STATUSES,
+      labelCol: { span: 6 },
+      wrapperCol: { span: 18 }
+    }
   }
 })
 </script>
 
 <style scoped lang="scss">
+.ant-form {
+  float: left;
+}
 </style>
