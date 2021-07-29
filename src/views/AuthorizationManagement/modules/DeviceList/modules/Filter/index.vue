ae832953c080b9d515d89531755e791c9e66843d
UILcoder <836219401@qq.com>
2021-07-29 13:40:13 +0800

    设备详情页面，授权状态和设备信息输入框提示文字

--- src/views/AuthorizationManagement/modules/DeviceList/modules/Filter/index.vue
+++ src/views/AuthorizationManagement/modules/DeviceList/modules/Filter/index.vue
@@ -1,20 +1,48 @@
 <template>
-  <a-card :bordered="false" class="jc-tabfilter">
-    <a-form layout="inline" :model="formState">
+  <a-card
+    :bordered="false"
+    class="jc-tabfilter"
+  >
+    <a-form
+      layout="inline"
+      :model="formState"
+    >
       <a-form-item label="创建日期">
-        <a-date-picker v-model:value="formState.createTime" format="YYYY-MM-DD" value-format="YYYY-MM-DD" :show-time="false" />
+        <a-range-picker
+          v-model:value="formState.createTime"
+          format="YYYY-MM-DD"
+          value-format="YYYY-MM-DD"
+          :show-time="false"
+        />
       </a-form-item>
       <a-form-item label="授权状态">
-        <a-select v-model:value="formState.licenseStatus">
-          <a-select-option v-for="item in DEVICE_LICENSE_STATUSES.VALUES" :key="item.value" :value="item.value">{{item.label}}</a-select-option>
+        <a-select
+          v-model:value="formState.licenseStatus"
+          placeholder="全部状态"
+        >
+          <a-select-option
+            v-for="item in DEVICE_LICENSE_STATUSES.VALUES"
+            :key="item.value"
+            :value="item.value"
+          >{{item.label}}</a-select-option>
         </a-select>
       </a-form-item>
       <a-form-item label="设备信息">
-        <a-input v-model:value="formState.equipmentNum"></a-input>
+        <a-input
+          v-model:value="formState.equipmentNum"
+          placeholder='请输入设备信息'
+        ></a-input>
       </a-form-item>
       <a-form-item class="jc-tabfilter-btns">
-        <a-button type="primary" html-type="submit" @click="onSubmit">查询</a-button>
-        <a-button class="jc-ml" @click="resetFields">重置</a-button>
+        <a-button
+          type="primary"
+          html-type="submit"
+          @click="onSubmit"
+        >查询</a-button>
+        <a-button
+          class="jc-ml"
+          @click="resetFields"
+        >重置</a-button>
       </a-form-item>
     </a-form>
   </a-card>
@@ -31,7 +59,7 @@ export default defineComponent({
   setup(props, { emit }) {
     let formState = reactive({
       createTime: '',
-      licenseStatus: '',
+      licenseStatus: undefined,
       equipmentNum: ''
     })
 
