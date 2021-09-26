8bc6d4c1f6fd5d2e5d4b0333f0183326d57d6051
lixiangyu <654762937@qq.com>
2021-09-26 15:27:34 +0800

    pkg更新

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,22 +1,13 @@
 <template>
   <div></div>
-  <!-- <img alt="Vue logo" src="@/assets/logo.png">
-  <img alt="Vue logo" src="../../assets/logo.png">
-  <img src="@/assets/icons/home.svg" alt="" style="width: 20px">
+  <img alt="Vue logo" src="@/assets/logo.png" />
+  <img alt="Vue logo" src="../../assets/logo.png" />
+  <img src="@/assets/icons/home.svg" alt="" style="width: 20px" />
   <div>
-    <a-button type="primary" @click="testVuex">
-      测试Vuex
-    </a-button>
+    <a-button type="primary" @click="testVuex"> 测试Vuex </a-button>
   </div>
-  <HelloWorld msg="Hello Vue 3.0 + Vite1" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite1" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite1" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite1" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite1" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite1" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite1" />
   <Messagea />
-  <div class="jc-svg-icon assd  s s s s ">
+  <div class="jc-svg-icon assd s s s s">
     <div>图标测试</div>
     <svg-icon name="home" />
     <svg-icon name="setting" />
@@ -24,38 +15,34 @@
       <vue-echarts :options="options" />
     </div>
     <div>
-      <a-button type="primary" @click="test">
-        测试
-      </a-button>
+      <a-button type="primary" @click="test"> 测试 </a-button>
     </div>
-  </div> -->
+  </div>
 </template>
 
 <script lang="ts">
-// import HelloWorld from '@/components/HelloWorld.vue'
-// import Messagea from '@/components/Messagea.vue'
+import Messagea from '@/components/Messagea.vue'
 import { computed, defineComponent, ref } from 'vue'
 
-// // import { GlobleState } from '@/store'
-// // import { useStore } from 'vuex'
-// import { useMyStore } from '@/hooks'
+// import { GlobleState } from '@/store'
+// import { useStore } from 'vuex'
+import { useMyStore } from '@/hooks'
 
 import VueEcharts, { OptionType } from '@/components/echarts'
 
 export default defineComponent({
   name: 'Home',
-  // components: {
-  //   HelloWorld,
-  //   Messagea,
-  //   VueEcharts
-  // },
+  components: {
+    Messagea,
+    VueEcharts
+  },
   setup() {
-    // console.log(import.meta.env.VITE_BASE_URL)
+    console.log(import.meta.env.VITE_BASE_URL)
     // const store = useStore<GlobleState>()
     // console.log(store.state.home.name)
-    // const { state, getters, commit, dispatch } = useMyStore()
+    const { state, getters, commit, dispatch } = useMyStore()
     // console.log('vuex state', state.user.loading)
-    // console.log('getters', getters['user/isLogin'])
+    console.log('getters', getters['user/isLogin'])
     const installData = ref([120, 200, 150, 80, 70, 110, 130])
     const options = computed(
       (): OptionType => ({
@@ -86,35 +73,35 @@ export default defineComponent({
         ]
       })
     )
-    // function test() {
-    //   console.log('test')
-    //   installData.value = [80, 70, 110, 130, 120, 200, 150]
-    // }
-    // function testVuex() {
-    //   console.log('testVuex')
-    //   // commit('user/GET_DATA', false)
-    //   dispatch('user/GET_DATA', false)
-    //   console.log('vuex state', state.user.loading)
-    //   setTimeout(() => {
-    //     console.log('vuex state', state.user.loading)
-    //   }, 3000)
-    // }
-    // return { options, test, testVuex }
+    function test() {
+      console.log('test')
+      installData.value = [80, 70, 110, 130, 120, 200, 150]
+    }
+    function testVuex() {
+      console.log('testVuex')
+      // commit('user/GET_DATA', false)
+      dispatch('user/GET_DATA', false)
+      // console.log('vuex state', state.user.loading)
+      // setTimeout(() => {
+      //   console.log('vuex state', state.user.loading)
+      // }, 3000)
+    }
+    return { options, test, testVuex }
   }
 })
 </script>
 <style lang="scss" scoped>
-// .jc-svg-icon {
-//   color: red;
-// }
-// .echarts {
-//   width: 300px;
-//   height: 200px;
-// }
-// ::v-deep(.deep-test) {
-//   color: red;
-//   .c1 {
-//     color: wheat;
-//   }
-// }
+.jc-svg-icon {
+  color: red;
+}
+.echarts {
+  width: 300px;
+  height: 200px;
+}
+::v-deep(.deep-test) {
+  color: red;
+  .c1 {
+    color: wheat;
+  }
+}
 </style>
